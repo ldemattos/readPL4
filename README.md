af1275b8bc891202b5bfd97a9870ff3215ba9322
Leonardo M. N. de Mattos <l@mattos.eng.br>
2019-01-09 08:53:01 -0200

    Kicking off this project!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # readPL4
-Describe and read ATP's PL4 binary data file format by means of reverse engineering.
+Describe and read ATP's (Alternative Transients Program) PL4 binary data file format by means of reverse engineering.
+
+The documentation of this project can be on its wiki pages:
+* [PISA's binary PL4 description](https://github.com/ldemattos/readPL4/wiki/PISA's-PL4-Format)
---
+++ readPL4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  readPL4.py
+#  https://github.com/ldemattos/readPL4
+#  
+#  Copyright 2019 Leonardo M. N. de Mattos <l@mattos.eng.br>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, 
+#  version 3.0.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+def main(args):
+	return 0
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
